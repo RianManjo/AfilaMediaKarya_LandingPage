e0b8238cec00acb20c30262627583274070f6408
unknown <rian.mizar04@gmail.com>
2024-05-08 18:50:14 +0700

    tidy code

--- js/main.js
+++ js/main.js
@@ -2,16 +2,13 @@
 var copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
 
-
-// Deklarasi variabel untuk menyimpan kode JavaScript
 var navScript = function() {
-    // Mengatur fungsi saat tombol hamburger diklik
+
     $('.hamburger').click(function() {
-        $('.menus').toggleClass('active'); // Toggle class 'active' untuk menampilkan atau menyembunyikan menu
+        $('.menus').toggleClass('active'); 
     });
 };
 
-// Memanggil fungsi saat dokumen siap
 $(document).ready(function() {
-    navScript(); // Memanggil fungsi yang telah dideklarasikan sebelumnya
+    navScript(); 
 });
